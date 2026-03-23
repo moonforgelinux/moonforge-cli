49ace4f5f4ccd647f28f57aed1310323ce4d847b
Emmanuele Bassi <ebassi@igalia.com>
2026-03-23 12:04:14 +0000

    feature: Handle missing features to query
    The "features" positional arguments must be optional, otherwise the
    `--list` option will not work.
    
    We can't just use ArgumentParser.print_help() here, because we don't
    have access to the ArgumentParser instance.

--- src/moonforgecli/feature.py
+++ src/moonforgecli/feature.py
@@ -3,6 +3,7 @@
 
 import argparse
 import os
+import sys
 
 from .log import AnsiEscape
 from .features import available_features, get_feature
@@ -18,7 +19,7 @@ def add_args(parser) -> None:
                         help="optional list of features to query")
 
 
-def run(options) -> 0:
+def run(options) -> int:
     if options.list:
         res = [str(AnsiEscape(text="Available features:", fg_color=AnsiEscape.BLUE_FG, mods=AnsiEscape.BOLD))]
         max_name_len = 0
@@ -29,6 +30,11 @@ def run(options) -> 0:
             pad = " ".ljust(max_name_len - len(f.name) + 1)
             res.append(f"- {name}:{pad}{f.description}")
         print("\n".join(res))
+        return 0
+
+    if len(options.features) == 0:
+        print(f"{AnsiEscape(text="usage:", fg_color=AnsiEscape.BLUE_FG, mods=AnsiEscape.BOLD)} moonforge feature FEATURE...")
+        return 1
 
     features = []
     for feat in options.features:
