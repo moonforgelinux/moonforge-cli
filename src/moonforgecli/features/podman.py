b0b87f72123845f09aac2b98fc8a7f21ef86d4b4
Emmanuele Bassi <ebassi@igalia.com>
2026-04-28 15:46:30 +0100

    feature: Add podman
    Just like the "docker" feature, but using meta-moonforge-podman.
    
    The podman feature conflicts with the docker one.

---
+++ src/moonforgecli/features/podman.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2026 Igalia S.L.
+# SPDX-License-Identifier: MIT
+
+from . import Feature, FeatureFragment, FeatureInclude
+
+
+PODMAN_FEATURE = Feature(name="podman",
+                         description="Container support using Podman",
+                         includes=[
+                           FeatureInclude("meta-moonforge", "kas/include/layer/meta-moonforge-podman.yml")
+                         ],
+                         conflicts=["docker"])
