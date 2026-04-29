bc7126f930299dcfb4dcdaa63ee30e84a51c45f8
Emmanuele Bassi <ebassi@igalia.com>
2026-04-29 16:27:14 +0100

    feature: Add meta-moonforge-graphics
    Use graphics-weston as the name, in case we add more graphics sessions
    in the future.

---
+++ src/moonforgecli/features/graphics_weston.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2026 Igalia S.L.
+# SPDX-License-Identifier: MIT
+
+from . import Feature, FeatureFragment, FeatureInclude
+
+
+GRAPHICS_WESTON_FEATURE = Feature(name="graphics-weston",
+                                  description="Graphics support, using Weston",
+                                  includes=[
+                                    FeatureInclude("meta-moonforge", "kas/include/layer/meta-moonforge-graphics.yml")
+                                  ])
