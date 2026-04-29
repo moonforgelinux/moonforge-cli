95ec8246ea8c036cb8ba2a934b2c443071903a96
Emmanuele Bassi <ebassi@igalia.com>
2026-04-29 16:35:18 +0100

    feature: Add WPE
    Since the WPE feature depends on the graphics layer, we need to make the
    WPE feature conflict with the Weston graphics layer.

--- src/moonforgecli/features/graphics_weston.py
+++ src/moonforgecli/features/graphics_weston.py
@@ -8,4 +8,5 @@ GRAPHICS_WESTON_FEATURE = Feature(name="graphics-weston",
                                   description="Graphics support, using Weston",
                                   includes=[
                                     FeatureInclude("meta-moonforge", "kas/include/layer/meta-moonforge-graphics.yml")
-                                  ])
+                                  ],
+                                  conflicts=['graphics-wpe'])
---
+++ src/moonforgecli/features/graphics_wpe.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2026 Igalia S.L.
+# SPDX-License-Identifier: MIT
+
+from . import Feature, FeatureFragment, FeatureInclude
+
+
+GRAPHICS_WPE_FEATURE = Feature(name="graphics-wpe",
+                               description="Web Platform for Embedded",
+                               includes=[
+                                 FeatureInclude("meta-moonforge", "kas/include/layer/meta-moonforge-graphics.yml"),
+                                 FeatureInclude("meta-moonforge", "kas/include/layer/meta-moonforge-wpe.yml"),
+                               ],
+                               local_conf=[
+                                 FeatureFragment(section="meta-moonforge-wpe",
+                                                 weight=30,
+                                                 key="WAYLAND_COG_LAUNCH_URL",
+                                                 value="http://10.0.2.2:8080"),
+                               ],
+                               conflicts=['graphics-weston'])
