3d0b71ec78cc931d014c75256015e918e1186be3
Emmanuele Bassi <ebassi@igalia.com>
2026-05-20 17:18:45 +0100

    Add versioned validation for project metadata
    Since we might want to change the project metadata across revisions of
    moonforge-cli, we should have a way to specify the version that
    initialised the project, and a way to parse the project metadata
    depending on that version. Using Rust as an example, the version is
    called "edition", and its identifier uses a "YYYY.N" format. We use that
    identifier to determine which fields are known and valid.

--- src/moonforgecli/project.py
+++ src/moonforgecli/project.py
@@ -27,16 +27,29 @@ MACHINE_META: dict[str, str] = {
     "MACHINE_NAME": "name",
 }
 
+EDITIONS: dict[str, list[str]] = {
+    "2026.1": [
+        "edition",
+        "name",
+        "machine",
+        "variables",
+        "vcs",
+    ],
+}
+
+EDITION_DEFAULT = "2026.1"
+
 
 class Project:
     """The metadata for a Moonforge project."""
-    def __init__(self, name: str, path: Path, machine: Machine, features: list[Feature], variables: dict[str, str], vcs: str | None = None):
-        self._name = name
-        self._path = path
-        self._machine = machine
-        self._features = features
-        self._variables = variables
-        self._vcs = vcs
+    def __init__(self, **kwargs):
+        self._edition = kwargs.get("edition", EDITION_DEFAULT)
+        self._name = kwargs.get("name")
+        self._path = kwargs.get("path")
+        self._machine = kwargs.get("machine")
+        self._features = kwargs.get("features", [])
+        self._variables = kwargs.get("variables", {})
+        self._vcs = kwargs.get("vcs", "none")
 
     @property
     def name(self) -> str:
@@ -68,6 +81,10 @@ class Project:
             return "none"
         return self._vcs
 
+    @property
+    def edition(self) -> str:
+        return self._edition
+
     def to_kas(self) -> str:
         kf = kas.KasFile()
         kf.add_include(repo=None, file="kas/include/repo/meta-moonforge.yml")
@@ -91,32 +108,64 @@ class Project:
                 data = tomllib.load(f)
             except Exception as err:
                 log.error(f"Invalid project at {path}: {err}")
-            name = data["project"]["name"]
-            machine = get_machine(data["project"]["machine"])
-            features = [get_feature(x) for x in data["project"].get("features", [])]
-            variables = {}
-            for variable in data["project"].get("variables", []):
-                try:
-                    key, value = variable.split("=", 1)
-                    variables[key] = value
-                except ValueError:
-                    log.warning(f"Invalid variable {variable} in configuration")
-            return cls(name, path.absolute(), machine, features, variables, "none")
+            edition = data["project"].get("edition", EDITION_DEFAULT)
+            if edition not in EDITIONS:
+                log.error(f"Invalid edition '{edition}' for the project file.")
+
+            def validate_fields(check, edition_id, valid_fields):
+                for field in check:
+                    if field not in valid_fields:
+                        log.error(f"Invalid field {field} for the given project edition {edition_id}")
+
+            validate_fields(data["project"], edition, EDITIONS[edition])
+
+            def get_2026_1_fields(source):
+                args = {}
+                for field in EDITIONS["2026.1"]:
+                    match field:
+                        case "machine":
+                            args["machine"] = get_machine(source["machine"])
+
+                        case "features":
+                            args["features"] = [get_feature(x) for x in source.get("features", [])]
+
+                        case "variables":
+                            variables = {}
+                            for var in source.get("variables", []):
+                                try:
+                                    key, value = var.split("=", 1)
+                                    variables[key] = value
+                                except ValueError:
+                                    log.warning(f"Invalid variable {var} in project.toml")
+                            args["variables"] = variables
+
+                        case _:
+                            args[field] = source.get(field)
+                return args
+
+            match edition:
+                case "2026.1":
+                    kwargs = get_2026_1_fields(data["project"])
+                case _:
+                    log.error(f"Unknown edition {edition}")
+
+            return cls(path=path, **kwargs)
 
     def to_toml(self) -> str:
         res = [
             "[project]",
-            f'name = "{self._name}"',
+            f'edition = "{self.edition}"',
+            f'name = "{self.name}"',
         ]
         if self._machine is not None:
-            res.append(f'machine = "{self._machine.name}"')
+            res.append(f'machine = "{self.machine.name}"')
         features = []
-        for feat in self._features:
+        for feat in self.features:
             features.append(f'"{feat.name}"')
         if len(features) > 0:
             res.append(f"features = [ {', '.join(features)} ]")
         variables = []
-        for key in self._variables:
+        for key in self.variables:
             variables.append(f'"{key}={self._variables[key]}"')
         if len(variables) > 0:
             res.append(f"variables = [ {', '.join(variables)} ]")
